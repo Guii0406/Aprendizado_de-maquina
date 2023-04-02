5af44eb46d0abb29c4e20f3b87383d11055c046f
unknown <brunoramosleo@gmail.com>
2023-04-02 15:16:32 -0300

    Conteudo de cascavel - puro e filtrado

--- dropLineJP.py
+++ dropLineJP.py
@@ -6,7 +6,7 @@ import numpy as np
 #foz = pd.read_excel("C:\\Users\\dagos\\OneDrive\\Desktop\\Aprendizado_de-maquina\\airbnb-foz.xlsx")
 
 # carrega a tabela
-tabela = pd.read_excel("C:\\Users\\dagos\\OneDrive\\Desktop\\Aprendizado_de-maquina\\airbnb-matinhos-1.xlsx")
+tabela = pd.read_excel("D:\\Importante\\Aprendizado_de-maquina\\airbnb-Cascavel-1.xlsx")
 
 # remove as linhas que contêm pelo menos uma célula vazia
 tabela_sem_vazios = tabela.dropna()
@@ -17,4 +17,4 @@ tabela_sem_vazios_e_total_sem_impostos = tabela_sem_vazios[tabela_sem_vazios['Pr
 # imprime a nova tabela sem as linhas com células vazias e sem o valor "(Total sem impostos)" na coluna "preço"
 print(tabela_sem_vazios_e_total_sem_impostos)
 
-tabela_sem_vazios_e_total_sem_impostos.to_excel('airbnb-praia-de-matinhos.xlsx')
+tabela_sem_vazios_e_total_sem_impostos.to_excel('airbnb-Cascavel-filtrado.xlsx')
