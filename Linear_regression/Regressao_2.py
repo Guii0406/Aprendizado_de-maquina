806c991eff78e3a51fc86456298e2ed1fd919d49
jpdagostin <dagostinjoaopedro@gmail.com>
2023-04-17 20:32:16 -0300

    Teste quadratico regressão xgboost

--- Linear_regression/Regressao_2.py
+++ Linear_regression/Regressao_2.py
@@ -1,3 +1,5 @@
+# https://towardsdatascience.com/xgboost-fine-tune-and-optimize-your-model-23d996fab663
+
 import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
@@ -13,14 +15,16 @@ y = df['Preço']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Define o modelo XGBoost com os hiperparâmetros especificados
-xg_reg = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.7, learning_rate=0.3, max_depth=8, alpha=10, n_estimators=400)
+xg_reg = xgb.XGBRegressor(objective='reg:squarederror')
+# , colsample_bytree=0.7, learning_rate=0.3, max_depth=8, alpha=10, n_estimators=400
 
 # Treina o modelo com os dados de treinamento
 xg_reg.fit(X_train, y_train)
 
 # Faz as previsões com o modelo treinado usando os dados de teste
 preds = xg_reg.predict(X_test)
-
+for itemt, itemp in zip( y_test.to_list(), preds.tolist()):
+    print(str(itemt), str(itemp))
 # Define as características do novo lugar
 novo_lugar = pd.DataFrame({'Tipo': ['Casa de campo'], 'Lugar': ['Campina Grande do Sul'], 'Avaliação': [4.91], 'Quantidade de avaliações': [32], 'Hospedes': [16], 'Quartos': [4], 'Camas': [12], 'Banheiros': [3]})
 
